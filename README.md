590bbfaf50a851432d59c80b92e5c6b88cb3cc81
GarvitSharma870 <garvitsharma870@gmail.com>
2022-08-22 16:14:52 +0530

    Music-Project-Commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Music Player.
+
+Music Player using MERN
+
+![alt text](https://mrjatt.fm/covers/webp/250/tfzai.webp "Vibes music web application")
+![alt text](https://i.ibb.co/CtSvzvd/image.png "Vibes music web application with libraries")
+
+
+
+Clone the repository and start testing out the application yourself!
+
+In the project directory, you can run:
+
+```
+npm install 
+```
+```
+npm start
+```
+
+## Happy Coding :)
---
+++ src/data.js
@@ -0,0 +1,88 @@
+import { v4 as uuidv4 } from "uuid";
+function chillHop() {
+	return [
+		{
+			name: "So High",
+			cover:
+				"https://mrjatt.fm/covers/webp/250/tfzai.webp",
+			artist: "Sidhu Moose Waala",
+			audio: "https://mrjatt.pw/md/48b79032d0760e2deb8b5ca034f8edad/rsdzv/Uchiyaan%20Gallan%20-%20(Mr-Jatt.com).mp3",
+			color: ["#EF8EA9", "#ab417f"],
+			id: uuidv4(),
+			active: true,
+		},
+		{
+			name: "Beaver Creek",
+			cover:
+				"https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
+			artist: "Aso, Middle School, Aviino",
+			audio: "https://mp3.chillhop.com/serve.php/?mp3=10075",
+			color: ["#205950", "#2ab3bf"],
+			id: uuidv4(),
+			active: false,
+		},
+		{
+			name: "Daylight",
+			cover:
+				"https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg",
+			artist: "Aiguille",
+			audio: "https://mp3.chillhop.com/serve.php/?mp3=9272",
+			color: ["#EF8EA9", "#ab417f"],
+			id: uuidv4(),
+			active: false,
+		},
+		{
+			name: "Keep Going",
+			cover:
+				"https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
+			artist: "Swørn",
+			audio: "https://mp3.chillhop.com/serve.php/?mp3=9222",
+			color: ["#CD607D", "#c94043"],
+			id: uuidv4(),
+			active: false,
+		},
+		{
+			name: "Nightfall",
+			cover:
+				"https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg",
+			artist: "Aiguille",
+			audio: "https://mp3.chillhop.com/serve.php/?mp3=9148",
+			color: ["#EF8EA9", "#ab417f"],
+			id: uuidv4(),
+			active: false,
+		},
+		{
+			name: "Reflection",
+			cover:
+				"https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
+			artist: "Swørn",
+			audio: "https://mp3.chillhop.com/serve.php/?mp3=9228",
+			color: ["#CD607D", "#c94043"],
+			id: uuidv4(),
+			active: false,
+		},
+		{
+			name: "Under the City Stars",
+			cover:
+				"https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
+			artist: "Aso, Middle School, Aviino",
+			audio: "https://mp3.chillhop.com/serve.php/?mp3=10074",
+			color: ["#205950", "#2ab3bf"],
+			id: uuidv4(),
+			active: false,
+		},
+		{
+			name: "295",
+			cover:
+				"https://cover.mr-jatt.im/thumb/498137/295-1.jpg",
+			artist: "Sidhu Moose Waala",
+			audio: "https://cdnsongs.com/music/data/Single_Track/202107/295/128/295_1.mp3",
+			color: ["#EF8EA9", "#ab417f"],
+			id: uuidv4(),
+			active: false,
+		},
+		//ADD MORE HERE
+	];
+}
+
+export default chillHop;
